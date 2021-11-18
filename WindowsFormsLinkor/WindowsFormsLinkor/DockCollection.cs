f2f65ebeaa964a24ad9158e296fb0dddf0c49f33
vladg <vladg@DESKTOP-CB3DP3M>
2021-11-18 21:10:47 +0400

    Сделал лабу

---
+++ WindowsFormsLinkor/WindowsFormsLinkor/DockCollection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsWarships
+{
+    /// <summary>
+    /// Класс-коллекция доков
+    /// </summary>
+    public class DockCollection
+    {
+        /// <summary>
+        /// Словарь (хранилище) с доками
+        /// </summary>
+        readonly Dictionary<string, Dock<Vehicle>> dockStages;
+        /// <summary>
+        /// Возвращение списка названий доков
+        /// </summary>
+        public List<string> Keys => dockStages.Keys.ToList();
+
+        /// <summary>
+        /// Ширина окна отрисовки
+        /// </summary>
+        private readonly int pictureWidth;
+        /// <summary>
+        /// Высота окна отрисовки
+        /// </summary>
+        private readonly int pictureHeight;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="pictureWidth"></param>
+        /// <param name="pictureHeight"></param>
+        public DockCollection(int pictureWidth, int pictureHeight)
+        {
+            dockStages = new Dictionary<string, Dock<Vehicle>>();
+            this.pictureWidth = pictureWidth;
+            this.pictureHeight = pictureHeight;
+            
+        }
+
+/// <summary>
+/// Добавление дока
+/// </summary>
+/// <param name="name">Название парковки</param>
+public void AddDock(string name)
+        {
+            if (dockStages.ContainsKey(name)) return;
+            dockStages.Add(name, new Dock<Vehicle>(pictureWidth, pictureHeight));
+        }
+        /// <summary>
+        /// Удаление дока
+        /// </summary>
+        /// <param name="name">Название парковки</param>
+        public void DelDock(string name)
+        {
+            if (dockStages.ContainsKey(name)) dockStages.Remove(name) ;
+        }
+        /// <summary>
+        /// Доступ к доку
+        /// </summary>
+        /// <param name="ind"></param>
+        /// <returns></returns>
+        public Dock<Vehicle> this[string ind]
+        {
+            get
+            {
+                if (dockStages.ContainsKey(ind)) return dockStages[ind];
+                else return null;
+            }
+        }
+    }
+}
--- WindowsFormsLinkor/WindowsFormsLinkor/FormDock.Designer.cs
+++ WindowsFormsLinkor/WindowsFormsLinkor/FormDock.Designer.cs
@@ -37,8 +37,15 @@ namespace WindowsFormsWarships
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDockName = new System.Windows.Forms.TextBox();
+            this.listBoxDocks = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonDelDock = new System.Windows.Forms.Button();
+            this.buttonAddDock = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDock)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBoxDock
@@ -52,27 +59,27 @@ namespace WindowsFormsWarships
             // 
             // buttonSetWarship
             // 
-            this.buttonSetWarship.Location = new System.Drawing.Point(767, 34);
+            this.buttonSetWarship.Location = new System.Drawing.Point(767, 232);
             this.buttonSetWarship.Name = "buttonSetWarship";
-            this.buttonSetWarship.Size = new System.Drawing.Size(90, 60);
+            this.buttonSetWarship.Size = new System.Drawing.Size(90, 50);
             this.buttonSetWarship.TabIndex = 1;
-            this.buttonSetWarship.Text = "Парковка боевого корабля";
+            this.buttonSetWarship.Text = "Добавить боевой корабль";
             this.buttonSetWarship.UseVisualStyleBackColor = true;
             this.buttonSetWarship.Click += new System.EventHandler(this.buttonSetWarship_Click);
             // 
             // buttonSetLinkor
             // 
-            this.buttonSetLinkor.Location = new System.Drawing.Point(770, 100);
+            this.buttonSetLinkor.Location = new System.Drawing.Point(767, 288);
             this.buttonSetLinkor.Name = "buttonSetLinkor";
-            this.buttonSetLinkor.Size = new System.Drawing.Size(87, 49);
+            this.buttonSetLinkor.Size = new System.Drawing.Size(87, 34);
             this.buttonSetLinkor.TabIndex = 2;
-            this.buttonSetLinkor.Text = "Парковка линкора";
+            this.buttonSetLinkor.Text = "Добавить линкор";
             this.buttonSetLinkor.UseVisualStyleBackColor = true;
             this.buttonSetLinkor.Click += new System.EventHandler(this.buttonSetLinkor_Click);
             // 
             // buttonTakeWarship
             // 
-            this.buttonTakeWarship.Location = new System.Drawing.Point(16, 94);
+            this.buttonTakeWarship.Location = new System.Drawing.Point(7, 76);
             this.buttonTakeWarship.Name = "buttonTakeWarship";
             this.buttonTakeWarship.Size = new System.Drawing.Size(75, 23);
             this.buttonTakeWarship.TabIndex = 3;
@@ -86,16 +93,16 @@ namespace WindowsFormsWarships
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.maskedTextBox);
             this.groupBox1.Controls.Add(this.buttonTakeWarship);
-            this.groupBox1.Location = new System.Drawing.Point(760, 155);
+            this.groupBox1.Location = new System.Drawing.Point(760, 328);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(97, 211);
+            this.groupBox1.Size = new System.Drawing.Size(97, 110);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 68);
+            this.label2.Location = new System.Drawing.Point(7, 50);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 6;
@@ -104,7 +111,7 @@ namespace WindowsFormsWarships
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 27);
+            this.label1.Location = new System.Drawing.Point(3, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(94, 13);
             this.label1.TabIndex = 5;
@@ -112,25 +119,86 @@ namespace WindowsFormsWarships
             // 
             // maskedTextBox
             // 
-            this.maskedTextBox.Location = new System.Drawing.Point(52, 65);
+            this.maskedTextBox.Location = new System.Drawing.Point(52, 50);
             this.maskedTextBox.Name = "maskedTextBox";
             this.maskedTextBox.Size = new System.Drawing.Size(30, 20);
             this.maskedTextBox.TabIndex = 4;
             // 
-            // FormPDock
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Доки:";
+            // 
+            // textBoxDockName
+            // 
+            this.textBoxDockName.Location = new System.Drawing.Point(6, 25);
+            this.textBoxDockName.Name = "textBoxDockName";
+            this.textBoxDockName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxDockName.TabIndex = 8;
+            // 
+            // listBoxDocks
+            // 
+            this.listBoxDocks.FormattingEnabled = true;
+            this.listBoxDocks.Location = new System.Drawing.Point(6, 80);
+            this.listBoxDocks.Name = "listBoxDocks";
+            this.listBoxDocks.Size = new System.Drawing.Size(100, 95);
+            this.listBoxDocks.TabIndex = 9;
+            this.listBoxDocks.SelectedIndexChanged += new System.EventHandler(this.listBoxDocks_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonDelDock);
+            this.groupBox2.Controls.Add(this.buttonAddDock);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBoxDockName);
+            this.groupBox2.Controls.Add(this.listBoxDocks);
+            this.groupBox2.Location = new System.Drawing.Point(760, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(118, 214);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            // 
+            // buttonDelDock
+            // 
+            this.buttonDelDock.Location = new System.Drawing.Point(10, 185);
+            this.buttonDelDock.Name = "buttonDelDock";
+            this.buttonDelDock.Size = new System.Drawing.Size(88, 23);
+            this.buttonDelDock.TabIndex = 7;
+            this.buttonDelDock.Text = "Удалить док";
+            this.buttonDelDock.UseVisualStyleBackColor = true;
+            this.buttonDelDock.Click += new System.EventHandler(this.buttonDelDock_Click);
+            // 
+            // buttonAddDock
+            // 
+            this.buttonAddDock.Location = new System.Drawing.Point(7, 51);
+            this.buttonAddDock.Name = "buttonAddDock";
+            this.buttonAddDock.Size = new System.Drawing.Size(90, 23);
+            this.buttonAddDock.TabIndex = 13;
+            this.buttonAddDock.Text = "Добавить док";
+            this.buttonAddDock.UseVisualStyleBackColor = true;
+            this.buttonAddDock.Click += new System.EventHandler(this.buttonAddDock_Click);
+            // 
+            // FormDock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(869, 450);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.buttonSetLinkor);
             this.Controls.Add(this.buttonSetWarship);
             this.Controls.Add(this.pictureBoxDock);
-            this.Name = "FormPDock";
+            this.Name = "FormDock";
             this.Text = "Док";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDock)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -145,5 +213,11 @@ namespace WindowsFormsWarships
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDockName;
+        private System.Windows.Forms.ListBox listBoxDocks;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonDelDock;
+        private System.Windows.Forms.Button buttonAddDock;
     }
 }
